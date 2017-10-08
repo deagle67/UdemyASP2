4575d3501dccd6641ba2aec49ca73bec792dc6cc
Neko <Neko@TABLET-AQ7D2IAM>
2017-10-09 00:30:50 +0200

    1

--- UdemyASP2/Models/IdentityModels.cs
+++ UdemyASP2/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace UdemyASP2.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
