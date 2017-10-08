e65ee0e371f4a4b9c95aa918758974a51808de6c
Neko <Neko@TABLET-AQ7D2IAM>
2017-10-09 00:54:33 +0200

    1

--- UdemyASP2/Models/Customer.cs
+++ UdemyASP2/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace UdemyASP2.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MemberShipType MemberShipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
 }
