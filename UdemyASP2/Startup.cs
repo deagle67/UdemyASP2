ad51e37bd1b4b239a71e5f4fe5c4e7d3d8fa0592
Neko <Neko@TABLET-AQ7D2IAM>
2017-10-08 23:19:30 +0200

    Ajoutez des fichiers projet.

---
+++ UdemyASP2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UdemyASP2.Startup))]
+namespace UdemyASP2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
