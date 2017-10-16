f37ed3f1a80e2cb374b1e20c8b355bdaa290dcef
Neko <Neko@TABLET-AQ7D2IAM>
2017-10-16 23:27:21 +0200

    Api Exo Movies

---
+++ UdemyASP2/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using UdemyASP2.Models;
+
+namespace UdemyASP2.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public byte? MemberShipTypeId { get; set; }
+    }
+}
---
+++ UdemyASP2/Dtos/MovieDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using UdemyASP2.Models;
+
+namespace UdemyASP2.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public int NumberInStock { get; set; }
+
+        public int GenreId { get; set; }
+    }
+}
