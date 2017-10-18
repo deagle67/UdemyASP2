9b16e2659950e9b933ecae3cacb8653707381da4
Neko <Neko@TABLET-AQ7D2IAM>
2017-10-18 15:55:44 +0200

    a

---
+++ dbo.AspNetRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'a903da35-e47c-46fe-ad9a-f51d2f7f24f8', N'CanManageMovies')
---
+++ dbo.AspNetUserRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'b08bb39b-05be-4d49-8638-f0f0f62ed341', N'a903da35-e47c-46fe-ad9a-f51d2f7f24f8')
