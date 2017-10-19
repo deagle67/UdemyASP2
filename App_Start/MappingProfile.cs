972c0cf601e6ecc0d56bda2c1cd0a1d1ee1c8a5b
Neko <Neko@TABLET-AQ7D2IAM>
2017-10-20 00:38:53 +0200

    test

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using UdemyASP2.Dtos;
+using UdemyASP2.Models;
+
+namespace UdemyASP2.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Customer
+
+            //CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
+
+            CreateMap<Customer, CustomerDto>();
+
+            CreateMap<CustomerDto, Customer>();
+
+            // Movie
+
+            CreateMap<Movie, MovieDto>();
+
+            CreateMap<MovieDto, Movie>();
+
+            // MembershipType
+
+            CreateMap<MemberShipType, MembershipTypeDto>();
+
+            // Genre
+
+            CreateMap<Genre, GenreDto>();
+        }
+    }
+}
