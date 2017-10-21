f22cc6bb6aacc1cefccf351e53bc130a700089a6
Neko <Neko@TABLET-AQ7D2IAM>
2017-10-21 18:57:16 +0200

    typeahead

--- UdemyASP2/App_Start/BundleConfig.cs
+++ UdemyASP2/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace UdemyASP2
                         "~/Scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.datatables.js",
-                        "~/Scripts/datatables/datatables.bootstrap.js"
+                        "~/Scripts/datatables/datatables.bootstrap.js",
+                        "~/Scripts/typeahead.bundle.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -28,6 +29,7 @@ namespace UdemyASP2
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
+                      "~/Content/typeahead.js",
                       "~/Content/site.css"));
         }
     }
